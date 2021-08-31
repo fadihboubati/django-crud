f6bcf0a0040f4ecf07ceb5372a25b020c1a599d9
fadiHB <eng.fadi.hb@gmail.com>
2021-08-31 13:21:09 +0300

    add some comments

--- fav_food/urls.py
+++ fav_food/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
-from fav_food.views import HomeView, DishDetailView, DishCreateView, DishUpdateView, DishDeleteView
+from fav_food.views import HomeView, DishDetailView, DishCreateView, DishUpdateView, DishDeleteView, AboutUsView
 
 urlpatterns = [
     # path('', HomeView.as_view(), name='home'),
     # path('<int:pk>/', DetailsView.as_view(), name='detail'),
     # path('new/', addFoodLovers.as_view(), name='add'),
 
-    path('',HomeView.as_view(),name='home'),
-    path('home/<int:pk>',DishDetailView.as_view(),name='dish_details'),
+    path('home/',HomeView.as_view(),name='home'),
+    path('dish/<int:pk>',DishDetailView.as_view(),name='dish_details'),
     path('dish/new',DishCreateView.as_view(),name='create_view'),
     path('dish/<int:pk>/update',DishUpdateView.as_view(),name='update_view'),
     path('dish/<int:pk>/delete',DishDeleteView.as_view(),name='delete_view'),
+    path('aboutUs/', AboutUsView.as_view(), name='aboutUs'),
 ]
--- fav_food/views.py
+++ fav_food/views.py
@@ -4,6 +4,10 @@ from django.urls import reverse_lazy
 
 from .models import Menu
 
+class AboutUsView(TemplateView):
+    template_name = 'about_us.html'
+    
+
 # Create your views here.
 class HomeView(ListView):
     template_name = 'home.html'
@@ -18,13 +22,15 @@ class DishCreateView(CreateView):
     model = Menu
     fields = ['user', 'name', 'fav_drink', 'fav_meal', 'eating_frequency_per_day', 'like_spicy']
 
-class DishUpdateView(CreateView):
-    template_name = 'update.html'
-    model = Menu
-    fields = ['user', 'name', 'fav_drink', 'fav_meal', 'eating_frequency_per_day', 'like_spicy']
-
 class DishDeleteView(DeleteView):
     template_name = 'delete.html'
     model = Menu
     fields = ['user', 'name', 'fav_drink', 'fav_meal', 'eating_frequency_per_day', 'like_spicy']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('home') # after click submit and delete, redirect to the 'home' endpoint
+
+class DishUpdateView(UpdateView):
+    template_name = 'update.html'
+    model = Menu
+    # fields = ['user', 'name', 'fav_drink', 'fav_meal', 'eating_frequency_per_day', 'like_spicy']
+    fields = '__all__'
+    # here we did not determind where should redirect after submit, so will go to ...
